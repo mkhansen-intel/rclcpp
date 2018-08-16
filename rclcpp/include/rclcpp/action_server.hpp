17cbaa0ea0b25815af4a7d6eb2ad1f4f9953e52f
Matthew Hansen <matthew.k.hansen@intel.com>
2018-08-16 16:03:57 -0700

    Changed template ServiceT to ActionT

--- rclcpp/include/rclcpp/action_server.hpp
+++ rclcpp/include/rclcpp/action_server.hpp
@@ -79,7 +79,7 @@ protected:
   std::shared_ptr<rcl_node_t> node_handle_;
 };
 
-template<typename ServiceT>
+template<typename ActionT>
 class ActionServer : public ActionServerBase
 {
 public:
@@ -88,28 +88,26 @@ public:
   ActionServer(
     std::shared_ptr<rcl_node_t> node_handle,
     const std::string & action_name,
-    AnyServiceCallback<ServiceT> request_callback,
-    AnyServiceCallback<ServiceT> cancel_callback,
+    AnyServiceCallback<ActionT> request_callback,
+    AnyServiceCallback<ActionT> cancel_callback,
     rcl_service_options_t & service_options,
 	std::shared_ptr<node_interfaces::NodeServicesInterface> node_services,
 	rclcpp::callback_group::CallbackGroup::SharedPtr group)
   : ActionServerBase(node_handle), request_callback_(request_callback), cancel_callback_(cancel_callback)
   {
-	  std::string request_service_name = "_request_" + action_name;
-    request_service_ = Service<ServiceT>::make_shared(node_handle, request_service_name, request_callback, service_options);
+    std::string request_service_name = "_request_" + action_name;
+    request_service_ = Service<ActionT>::make_shared(node_handle, request_service_name, request_callback, service_options);
     auto req_base_ptr = std::dynamic_pointer_cast<ServiceBase>(request_service_);
     node_services->add_service(req_base_ptr, group);
 
     std::string cancel_service_name = "_cancel_" + action_name;
-    cancel_service_ = Service<ServiceT>::make_shared(node_handle, cancel_service_name, cancel_callback, service_options);
+    cancel_service_ = Service<ActionT>::make_shared(node_handle, cancel_service_name, cancel_callback, service_options);
     auto cancel_base_ptr = std::dynamic_pointer_cast<ServiceBase>(cancel_service_);
     node_services->add_service(cancel_base_ptr, group);
 
     //std::string feedback_topic_name = "_feedback_" + action_name;
-
     // TODO: Add feedback publisher
     //feedback_publisher_ =
-    // TODO: Create simple example / test for Action Server
   }
   
   ActionServer() = delete;
@@ -144,24 +142,24 @@ public:
 
   void send_request_response(
     std::shared_ptr<rmw_request_id_t> req_id,
-    std::shared_ptr<typename ServiceT::Response> response)
+    std::shared_ptr<typename ActionT::Response> response)
   {
     request_service_->send_response(req_id, response);
   }
 
   void send_cancel_response(
     std::shared_ptr<rmw_request_id_t> req_id,
-    std::shared_ptr<typename ServiceT::Response> response)
+    std::shared_ptr<typename ActionT::Response> response)
   {
 	cancel_service_->send_response(req_id, response);
   }
 
 private:
   RCLCPP_DISABLE_COPY(ActionServer)
-  std::shared_ptr<Service<ServiceT>> request_service_;
-  std::shared_ptr<Service<ServiceT>> cancel_service_;
-  AnyServiceCallback<ServiceT> request_callback_;
-  AnyServiceCallback<ServiceT> cancel_callback_;
+  std::shared_ptr<Service<ActionT>> request_service_;
+  std::shared_ptr<Service<ActionT>> cancel_service_;
+  AnyServiceCallback<ActionT> request_callback_;
+  AnyServiceCallback<ActionT> cancel_callback_;
 };
 
 }  // namespace rclcpp
